df581c5b3425d3b8fe2751737877fce382422950
Micheal Quentin <mquenti3@gmail.com>
2024-04-01 16:00:44 -0500

    Initial GUI

--- translator.py
+++ translator.py
@@ -1,20 +1,30 @@
 from googletrans import Translator
 import sys
+from tkinter import*
 
-def main():
+# Create GUI
+window = Tk()
+
+input_field = Entry(window, width=50)
+input_field.pack()
+
+def translate():
+    # Translate the text
     try:
         translator = Translator()
-        input_text = input("Enter the text you want to translate: ")
+        output_string = "Spanish: " + translator.translate(input_field.get(), dest='es').text
+        Label(window, text=output_string).pack()
 
-        # Translate the text to German
-        print("Spanish:", translator.translate(input_text, dest='es').text)
-        print("French:", translator.translate(input_text, dest='fr').text)
-        print("Portuguese:", translator.translate(input_text, dest='pt').text)
-        print("Italian:", translator.translate(input_text, dest='it').text)
-
+        # Languages
+        #print("Spanish:", translator.translate(input_text, dest='es').text)
+        #print("French:", translator.translate(input_text, dest='fr').text)
+        #print("Portuguese:", translator.translate(input_text, dest='pt').text)
+        #print("Italian:", translator.translate(input_text, dest='it').text)
     except Exception as e:
         print("An error occurred:", e)
         sys.exit(1)
 
-if __name__ == "__main__":
-    main()
+translateButton = Button(window, text="Translate", command=translate)
+translateButton.pack()
+
+window.mainloop()
