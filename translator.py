d61c58cbf1482f2a686ff3445915962c38476181
Micheal Quentin <mquenti3@gmail.com>
2024-04-01 14:00:07 -0500

    Translate into Several Languages

--- translator.py
+++ translator.py
@@ -1,3 +1,20 @@
-import googletrans
+from googletrans import Translator
+import sys
 
-print(googletrans.LANGUAGES)
+def main():
+    try:
+        translator = Translator()
+        input_text = input("Enter the text you want to translate: ")
+
+        # Translate the text to German
+        print("Spanish:", translator.translate(input_text, dest='es').text)
+        print("French:", translator.translate(input_text, dest='fr').text)
+        print("Portuguese:", translator.translate(input_text, dest='pt').text)
+        print("Italian:", translator.translate(input_text, dest='it').text)
+
+    except Exception as e:
+        print("An error occurred:", e)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
