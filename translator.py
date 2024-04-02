a5139205ecd76adf5566eed0c50b8ef865d4eed5
Micheal Quentin <mquenti3@gmail.com>
2024-04-02 07:51:08 -0500

    Translate Multiple Languages At Once

--- translator.py
+++ translator.py
@@ -2,6 +2,7 @@ import googletrans
 from googletrans import Translator
 import sys
 from tkinter import*
+import tkinter.ttk as ttk
 
 # Create GUI object
 window = Tk()
@@ -10,40 +11,74 @@ window = Tk()
 translator = Translator()
 language_keys = list(googletrans.LANGUAGES.keys())
 languages = list(googletrans.LANGUAGES.values())
+languages = [word.title() for word in languages]
 #print(languages)
 
+# Create Fields
+fields = list()
+class Field:
+    def __init__(self, dropdown, entry):
+        self.dropdown = dropdown
+        self.entry = entry
+
+# Add field
+def add_entry():
+    dropdown = ttk.Combobox(window, value=languages)
+    dropdown.current(21)
+    dropdown.grid(row=len(fields), column=0)
+    entry = Entry(window, width=100)
+    entry.grid(row=len(fields), column=1)
+    newField = Field(dropdown, entry)
+    fields.append(newField)
+
+# Remove field
+def remove_entry():
+    if fields.len() > 0:
+        fields[len(fields)-1].dropdown.destroy()
+        fields[len(fields)-1].entry.destroy()
+        fields[len(fields)-1].pop()
+
 # Create Field1
-selected1 = StringVar()
-selected1.set("english")
-dropdown1 = OptionMenu(window, selected1, *languages)
-dropdown1.pack(side=LEFT)
-entry1 = Entry(window, width=50)
-entry1.pack(side=LEFT)
+#dropdown1 = ttk.Combobox(window, value=languages)
+#dropdown1.current(21)
+#dropdown1.grid(row=0, column=0)
+#entry1 = Entry(window, width=100)
+#entry1.grid(row=0, column=1)
 
 # Create Field2
-selected2 = StringVar()
-selected2.set("english")
-dropdown2 = OptionMenu(window, selected2, *languages)
-dropdown2.pack(side=LEFT)
-entry2 = Entry(window, width=50)
-entry2.pack(side=LEFT)
+#dropdown2 = ttk.Combobox(window, value=languages)
+#dropdown2.current(21)
+#dropdown2.grid(row=1, column=0)
+#entry2 = Entry(window, width=100)
+#entry2.grid(row=1, column=1)
+        
+# Create the first two fields
+add_entry()
+add_entry()
+add_entry()
+add_entry()
 
+# Translate the text
 def translate():
-    # Translate the text
-    try:
-        entry2.delete(0, 'end')
-        entry2.insert(0, translator.translate(entry1.get(), src=language_keys[languages.index(selected1.get())], dest=language_keys[languages.index(selected2.get())]).text)
-    except Exception as e:
-        print("FATAL ERROR:", e)
-        sys.exit(1)
+    index = 1
+    if len(fields) > 1 and fields[0].entry.get() != '':
+        while index < len(fields):
+            try:
+                fields[index].entry.delete(0, 'end')
+                fields[index].entry.insert(0, translator.translate(fields[0].entry.get(), src=language_keys[languages.index(fields[0].dropdown.get())], dest=language_keys[languages.index(fields[index].dropdown.get())]).text)
+                index += 1
+            except Exception as e:
+                print("FATAL ERROR:", e)
+                sys.exit(1)
+        
 
 def clear_text():
-    entry1.delete(0, 'end')
-    entry2.delete(0, 'end')
+    for field in fields:
+        field.entry.delete(0, 'end')
 
 translateButton = Button(window, text="Translate", command=translate)
-translateButton.pack()
+translateButton.grid(row=len(fields), column=1)
 clearButton = Button(window, text="Clear", command=clear_text)
-clearButton.pack()
+clearButton.grid(row=len(fields), column=2)
 
 window.mainloop()
